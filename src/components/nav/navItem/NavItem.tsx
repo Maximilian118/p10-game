8d9b8640bf4f1f817c3b246b316e7d9ee84aea60
Maximilian118 <maxcrosby118@gmail.com>
2024-04-02 12:48:51 +0100

    Ripple

--- src/components/nav/navItem/NavItem.tsx
+++ src/components/nav/navItem/NavItem.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import "./_navItem.scss"
 
 interface navItemProps {
   item: {
@@ -11,14 +12,33 @@ interface navItemProps {
 }
 
 const NavItem: React.FC<navItemProps> = ({ item, style, setValue }) => {
-  const onClickHandler = (e: React.MouseEvent<HTMLElement>, setValue: Function) => {
-    const navLeft = document.getElementsByTagName("nav")[0].getBoundingClientRect().left
+  const createRipple = (e: React.MouseEvent<HTMLElement>, navPos: DOMRect): void => {
+    const item = e.currentTarget
+
+    const circle = document.createElement("span")
+    const diameter = Math.max(item.clientWidth, item.clientHeight)
+    const radius = diameter / 2
+
+    circle.addEventListener('animationend', () => item.removeChild(circle))
+
+    circle.style.width = circle.style.height = `${diameter}px`
+    circle.style.left = `${e.pageX - (item.offsetLeft + radius) - navPos.left}px`
+    circle.style.top = `${e.pageY - (item.offsetTop + radius) - navPos.top}px`
+    circle.classList.add("ripple")
+
+    item.appendChild(circle)
+  }
+
+  const onClickHandler = (e: React.MouseEvent<HTMLElement>, setValue: Function): void => {
+    const navPos = document.getElementsByTagName("nav")[0].getBoundingClientRect()
     const leftPos = e.currentTarget.getBoundingClientRect().left
+
+    createRipple(e, navPos)
 
     setValue({
       index: item.index,
       itemWidth: e.currentTarget.clientWidth,
-      leftPosition: leftPos - navLeft,
+      leftPosition: leftPos - navPos.left,
     })
   }
 
