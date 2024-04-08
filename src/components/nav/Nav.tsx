4437f9e6969b84ae6648b77cf4d3fae08da2b234
Maximilian118 <maxcrosby118@gmail.com>
2024-04-08 23:33:34 +0100

    Simplification

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,23 +1,11 @@
-import React, { useState } from "react"
+import React from "react"
 import NavItem from './navItem/NavItem'
 
-interface navType {
-  index: number,
-  itemWidth: number,
-  leftPosition: number,
-}
-
 const Nav: React.FC = () => {
-  const [value, setValue] = useState<navType>({
-    index: 0,
-    itemWidth: 0,
-    leftPosition: 0,
-  })
-
   const navItems = [
     {
       text: "Home",
-      url: "/home",
+      url: "/",
     },
     {
       text: "Championships",
@@ -27,8 +15,7 @@ const Nav: React.FC = () => {
 
   return (
     <nav>
-      {navItems.map((item, i) => (<NavItem key={i} setValue={setValue} item={{...item, index: i}}/>))}
-      <span className="nav-item-indicator" style={{ width: value.itemWidth, left: value.leftPosition }}/>
+      {navItems.map((item, i) => (<NavItem key={i} item={{...item, i}}/>))}
     </nav>
   )
 }
--- src/components/nav/navItem/NavItem.tsx
+++ src/components/nav/navItem/NavItem.tsx
@@ -1,49 +1,25 @@
 import React from "react"
 import "./_navItem.scss"
+import { useLocation, useNavigate } from "react-router-dom"
 
 interface navItemProps {
   item: {
     text: string,
     url: string,
-    index: number,
+    i: number,
   },
-  setValue: Function,
-  style?: object,
 }
 
-const NavItem: React.FC<navItemProps> = ({ item, style, setValue }) => {
-  const createRipple = (e: React.MouseEvent<HTMLElement>, navPos: DOMRect): void => {
-    const item = e.currentTarget
-
-    const circle = document.createElement("span")
-    const diameter = Math.max(item.clientWidth, item.clientHeight)
-    const radius = diameter / 2
-
-    circle.addEventListener('animationend', () => item.removeChild(circle))
- 
-    circle.style.width = circle.style.height = `${diameter}px`
-    circle.style.left = `${e.pageX - (item.offsetLeft + radius) - navPos.left}px`
-    circle.style.top = `${e.pageY - (item.offsetTop + radius) - navPos.top}px`
-    circle.classList.add("ripple")
-
-    item.appendChild(circle)
-  }
-
-  const onClickHandler = (e: React.MouseEvent<HTMLElement>, setValue: Function): void => {
-    const navPos = document.getElementsByTagName("nav")[0].getBoundingClientRect()
-    const leftPos = e.currentTarget.getBoundingClientRect().left
-
-    createRipple(e, navPos)
-
-    setValue({
-      index: item.index,
-      itemWidth: e.currentTarget.clientWidth,
-      leftPosition: leftPos - navPos.left,
-    })
-  }
+const NavItem: React.FC<navItemProps> = ({ item }) => {
+  const location = useLocation()
+  const navigate = useNavigate()
 
   return (
-    <div className="nav-item" style={style} onClick={e => onClickHandler(e, setValue)}>
+    <div 
+      className="nav-item" 
+      style={{ color: location.pathname === item.url ? "black" : ""}} 
+      onClick={() => navigate(item.url)}
+    >
       <p>{item.text}</p>
     </div>
   )
