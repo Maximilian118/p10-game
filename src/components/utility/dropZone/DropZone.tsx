c01c546b5c0c5eab1028b56799ea9b311c662644
Maximilian118 <maxcrosby118@gmail.com>
2024-04-11 18:15:22 +0100

    DropZone style and Thumbnail

--- src/components/utility/dropZone/DropZone.tsx
+++ src/components/utility/dropZone/DropZone.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useDropzone } from "react-dropzone"
 import { compressImage } from "./dropZoneUtility"
 import './_dropZone.scss'
@@ -8,12 +8,19 @@ interface dropZoneType<T> {
   setFormErr: React.Dispatch<React.SetStateAction<T>>,
 }
 
-interface compressImagesType {
+interface formType {
   icon: File | null,
   profile_picture: File | null,
 }
 
-const DropZone = <T extends compressImagesType>({ setForm, setFormErr }: dropZoneType<T>) => { 
+interface compressedImagesType {
+  icon: File,
+  profile_picture: File,
+}
+
+const DropZone = <T extends formType>({ setForm, setFormErr }: dropZoneType<T>) => {
+  const [ thumb, setThumb ] = useState<string>("")
+
   // Determine if the window has drag and drop capabilities.
   const canDragDrop = (): boolean => {
     const testDiv = document.createElement('div')
@@ -39,7 +46,7 @@ const DropZone = <T extends compressImagesType>({ setForm, setFormErr }: dropZon
         setForm: React.Dispatch<React.SetStateAction<T>>,
         setFormErr: React.Dispatch<React.SetStateAction<T>>,
       ): Promise<void> => {
-        const compressImages = async (file: File): Promise<compressImagesType> => {
+        const compressImages = async (file: File): Promise<compressedImagesType> => {
           return {
             icon: await compressImage(file, 0.1),
             profile_picture: await compressImage(file, 1),
@@ -47,6 +54,8 @@ const DropZone = <T extends compressImagesType>({ setForm, setFormErr }: dropZon
         }
 
         const compressedImages = await compressImages(acceptedFiles[0])
+
+        setThumb(URL.createObjectURL(compressedImages.profile_picture))
 
         setForm(prevForm => {
           return {
@@ -60,10 +69,23 @@ const DropZone = <T extends compressImagesType>({ setForm, setFormErr }: dropZon
     }
   }, [acceptedFiles, setForm, setFormErr])
 
+  const dropZoneContent = (
+    canDragDrop: () => boolean,
+    thumb: string,
+  ): JSX.Element => {
+    if (thumb) {
+      return <img alt="Thumbnail" src={thumb}/>
+    }
+    
+    return <p>{`${canDragDrop() ? `Drag and drop` : `Select`} an image...`}</p>
+  }
+
   return (
     <div {...getRootProps({className: 'dropzone'})}>
-      <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
+      <div className="inside-border">
+        <input {...getInputProps()} />
+        {dropZoneContent(canDragDrop, thumb)}
+      </div>
     </div>
   )
 }
