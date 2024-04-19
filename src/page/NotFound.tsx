47487135169e14000e80af73d8cf8969c8949d66
Maximilian118 <maxcrosby118@gmail.com>
2024-04-19 01:56:27 -0300

    600px main width

--- src/page/NotFound.tsx
+++ src/page/NotFound.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const NotFound: React.FC = () => (
   <div className="content-container">
-    <img alt="Page not found." src="https://i.imgflip.com/4aq4be.jpg"/>
+    <img alt="Page not found." src="https://i.imgflip.com/4aq4be.jpg" style={{ width: "80%" }}/>
   </div>
 )
 
