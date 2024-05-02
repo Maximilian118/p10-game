e7bf76dc067b0f022ea2fe09fc5a64781dd9e667
Maximilian118 <maxcrosby118@gmail.com>
2024-05-01 21:52:09 -0400

    Basic splash

--- src/page/Splash.tsx
+++ src/page/Splash.tsx
@@ -1,9 +1,23 @@
 import React from "react"
+import formBackground from '../assets/forms/f1-car2.jpeg'
+import { useNavigate } from "react-router-dom"
 
 const Splash: React.FC = () => {
+  const navigate = useNavigate()
+
   return (
     <div className="content-container">
-      
+      <div className="form-container">
+        <img 
+          src={formBackground} 
+          alt="An old Formula 1 car." 
+          className="form-background"
+          style={{ left: 0 }}
+        />
+        <div className="form-title" style={{ marginBottom: 200 }}>
+          <h2 className="clickable" onClick={() => navigate("/login")}>Go to Login</h2>
+        </div>
+      </div>
     </div>
   )
 }
